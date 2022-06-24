33ede9bd9ce245d70ad2aff24718733bb4836eb0
Vishwas <baghel.vishwas0@gmail.com>
2022-06-24 12:29:52 +0530

    [Vishwas]add: Finding Words Frequency In Paragraph

--- HashTableProgram/Program.cs
+++ HashTableProgram/Program.cs
@@ -7,23 +7,41 @@ namespace HashTableProgram
         {
             Console.WriteLine("Welcome to the HashTable Program \n================================");
             LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
-            string sentence = " to be or not to be";
-            string[] words = sentence.ToLower().Split(" ");
-            foreach( string word in words)
+            //string sentence = " to be or not to be";
+            //string[] words = sentence.ToLower().Split(" ");
+            //foreach( string word in words)
+            //{
+            //    int value = linkedHashMap.Get(word);
+            //    if (value == default)
+            //    {
+            //        value = 1;
+            //    }
+            //    else
+            //    {
+            //        value = +1;
+            //    }
+            //    linkedHashMap.Add(word, value);
+            //}
+            //int frequency = linkedHashMap.Get("not");
+            //Console.WriteLine(frequency);
+
+            string Paragraph = "“Paranoids are not paranoid because they are " +
+              "paranoid but because they keep putting themselves deliberately into" +
+              " paranoid avoidable situations";
+            string[] letters = Paragraph.ToLower().Split(" ");
+
+            foreach (string word in letters)
             {
                 int value = linkedHashMap.Get(word);
                 if (value == default)
                 {
                     value = 1;
                 }
-                else
-                {
-                    value = +1;
-                }
+                else value += 1;
                 linkedHashMap.Add(word, value);
             }
-            int frequency = linkedHashMap.Get("not");
-            Console.WriteLine(frequency);
+            int frequency1 = linkedHashMap.Get("paranoid");
+            Console.WriteLine(frequency1);
         }
     }
 }
