9e5754c7eda76b56c9ccf4d10f60ed3a723d2dac
Vishwas <baghel.vishwas0@gmail.com>
2022-06-24 12:22:25 +0530

    [Vishwas]add: Finding Words Frequency In A Sentence Program Added

---
+++ HashTableProgram/LinkedList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableProgram
+{
+    class LinkedList<K, V> where K : IComparable
+    {
+
+        public MyMapNode<K, V> head;
+        public MyMapNode<K, V> tail;
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+        }
+        public MyMapNode<K, V> Search(K key)
+        {
+
+            MyMapNode<K, V> temp = head;
+            while (temp != null)
+            {
+                if (temp.key.Equals(key))
+                    return temp;
+                temp = temp.next;
+            }
+            return null;
+        }
+        public void Append(MyMapNode<K, V> node)
+        {
+
+            if (head == null && tail == null)
+            {
+                head = node;
+                tail = node;
+            }
+            else
+            {
+
+                tail.next = node;
+                tail = node;
+            }
+        }
+        public bool IsEmpty()
+        {
+            return head == null && tail == null;
+        }
+
+        public MyMapNode<K, V> Pop()
+        {
+
+            MyMapNode<K, V> temp = head;
+            if (head != null)
+            {
+
+                head = head.next;
+            }
+            return temp;
+        }
+
+        public MyMapNode<K, V> PopLast()
+        {
+            MyMapNode<K, V> tailTemp = tail;
+            if (!IsEmpty())
+            {
+
+                MyMapNode<K, V> temp = head;
+                while (temp.next != tail)
+                {
+
+                    temp = temp.next;
+                }
+
+                temp.next = null;
+                tail = temp;
+            }
+            return tailTemp;
+        }
+
+        public bool DeleteNode(MyMapNode<K, V> DeleteNode)
+        {
+
+            MyMapNode<K, V> temp = head;
+            if (!IsEmpty())
+            {
+
+                if (DeleteNode.key.Equals(head.key))
+                {
+                    Pop();
+                    return true;
+                }
+
+                if (DeleteNode.key.Equals(tail.key))
+                {
+                    PopLast();
+                    return true;
+                }
+                while (temp != null)
+                {
+
+                    if (temp.next != null && temp.next.key.Equals(DeleteNode.key))
+                    {
+                        temp.next = DeleteNode.next;
+                        return true;
+                    }
+                    temp = temp.next;
+                }
+            }
+            return false;
+        }
+
+
+    }
+}
--- HashTableProgram/Program.cs
+++ HashTableProgram/Program.cs
@@ -6,6 +6,24 @@ namespace HashTableProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the HashTable Program \n================================");
+            LinkedHashMap<string, int> linkedHashMap = new LinkedHashMap<string, int>(5);
+            string sentence = " to be or not to be";
+            string[] words = sentence.ToLower().Split(" ");
+            foreach( string word in words)
+            {
+                int value = linkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else
+                {
+                    value = +1;
+                }
+                linkedHashMap.Add(word, value);
+            }
+            int frequency = linkedHashMap.Get("not");
+            Console.WriteLine(frequency);
         }
     }
 }
